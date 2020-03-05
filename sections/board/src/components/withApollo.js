ee10928550618da81f339e44137db83c274db650
Johnston Tyler USGR <tyler.johnston@syngenta.com>
2020-03-05 16:43:08 -0500

    update window

--- sections/board/src/components/withApollo.js
+++ sections/board/src/components/withApollo.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
+const windowGlobal = typeof window !== 'undefined' && window
+
 const client = new ApolloClient({
-    uri: `${window.location.origin}/api/graphql`
+    uri: `${windowGlobal.location.origin}/api/graphql`
 });
 
 function withApollo(Component) {
