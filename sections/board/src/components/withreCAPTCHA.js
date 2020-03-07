0faeb0ebd0cb7b15e5d11b10243e3db13f06b88e
Johnston Tyler USGR <tyler.johnston@syngenta.com>
2020-03-07 14:23:14 -0500

    feat(reCAPTCHA): get click handler working

--- sections/board/src/components/withreCAPTCHA.js
+++ sections/board/src/components/withreCAPTCHA.js
@@ -1,22 +1,21 @@
 import React from 'react';
 import {
     GoogleReCaptchaProvider,
-    useGoogleReCaptcha
 } from 'react-google-recaptcha-v3';
 
 function WithreCAPTCHA(WrappedComponent, props) {
-    console.log(props);
 
-    const { executeRecaptcha } = useGoogleReCaptcha();
-    if (executeRecaptcha) {
-        const token = executeRecaptcha(props.action);
-        console.log(token);
+    const updatedProps = {
+        ...props,
+        reCAPTCHA: {
+            action: props.action
+        }
     }
 
     return (
         <GoogleReCaptchaProvider 
             reCaptchaKey="6LdNgN8UAAAAANhDlaf09EQLyBbzoJjNW4M1O4S7">
-            {<WrappedComponent />}
+            <WrappedComponent {...updatedProps}/>
         </GoogleReCaptchaProvider> 
     );
 };
