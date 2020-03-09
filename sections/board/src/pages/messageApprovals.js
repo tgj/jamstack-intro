c6ec81f8237b2e9db34835fed03a0e975b140a18
Johnston Tyler USGR <tyler.johnston@syngenta.com>
2020-03-09 14:15:45 -0400

    Get approvals working locally

--- sections/board/src/pages/messageApprovals.js
+++ sections/board/src/pages/messageApprovals.js
@@ -1,19 +1,21 @@
-import React, { useReducer } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import '../global.css';
+import styles from './ma.module.css';
 import withApollo from '../components/withApollo';
 import { useMutation } from '@apollo/react-hooks';
 import { gql } from 'apollo-boost';
+import { extractGraphQlErrors } from '../lib/util';
 
-const APPROVAL_MESSAGE = gql`
-    mutation ApproveMessage($input: ApproveMessageInput!) {
+const APPROVE_MESSAGE = gql`
+    mutation ApproveMessage($input: MessageApprovalInput!) {
         approveMessage(input: $input) {
-            approvalDate
+            approved
         }
     }
 `;
 
 const INITIAL_STATE = {
-    status: 'IDLE'
+    status: 'PENDING'
 };
 
 const reducer = (state, action) => {
@@ -30,19 +32,69 @@ const reducer = (state, action) => {
 
 const MessageApprovals = () => {
     const [state, dispatch] = useReducer(reducer, INITIAL_STATE);
+    const [approveMessage, { data }] =  useMutation(APPROVE_MESSAGE);
 
-    /*const url = window.location;
-    const urlObject = new URL(url);
-    const code = urlObject.searchParams.get('code');*/
+    const setStatus = status => dispatch({
+        type: 'updateStatus',
+        status
+    });
+
+    useEffect(() => {
+        const url = window.location;
+        const urlObject = new URL(url);
+        const code = urlObject.searchParams.get('code');
+        if (code) {
+            async function approve() {
+                await approveMessage({ variables: { 
+                    input: {
+                        approvalId: code,
+                    }
+                }})
+                .then(response => {
+                    console.log(response);
+                    const approved = response.data.approveMessage.approved;
+                    if (approved) {
+                        setStatus('APPROVED');
+                    } else {
+                        setStatus('REJECT');
+                    }
+                })
+                .catch (error => {
+                    const errors = extractGraphQlErrors(error)
+                    dispatch({
+                        type: 'updateStatus',
+                        status: 'ERROR',
+                        errors
+                    })
+                });
+            }
+            approve();
+        }
+    }, [approveMessage]);
 
     return (
         <>
+        <div>
             <h1>Message Approval</h1>
-            {state.status === 'SUCCESS' && (
-                <p>
-                    Message approved!
-                </p>
-            )}
+            <p className={styles.centered}>
+                   {state.status === 'PENDING' &&
+                       'Approving Message...'
+                   }
+                    {state.status === 'APPROVED' &&
+                       'Message Approved! ✅'
+                   }
+                    {state.status === 'REJECT' &&
+                       'Message Not Approved! ⚠️'
+                   }
+            </p>
+            {
+                (state.status === 'APPROVED' || state.status === 'REJECT') && (
+                    <p className={`${styles.centered} ${styles.closeMessage}`}>
+                        You may close this browser window.
+                    </p>
+                )
+            }
+        </div>
         </>
     )
 };
